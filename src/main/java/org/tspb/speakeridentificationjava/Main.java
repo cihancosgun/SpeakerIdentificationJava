89f843bb5b8672323b5fe30f633ac492a716e8f4
ccosgun@tspb.org.tr <ccosgun@tspb.org.tr>
2020-09-21 11:27:08 +0300

    init commit

---
+++ src/main/java/org/tspb/speakeridentificationjava/Main.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.tspb.speakeridentificationjava;
+
+import org.tspb.speakeridentificationjava.gui.UserInterface;
+
+/**
+ *
+ * @author cihan
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        UserInterface.showGui();
+    }
+}
